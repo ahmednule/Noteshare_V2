2ee30d35cfd0956dad89238aef8a4f97aa798984
ahmednule <nuleahmed6@gmail.com>
2024-07-10 13:50:16 +0300

    These are the templates

--- Noteshare_v2/homeapp/views.py
+++ Noteshare_v2/homeapp/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    render(request, 'homeapp/home.html')
+    return render(request, 'homeapp/home.html')
 
 def about(request):
-    render(request, 'homeapp/about.html')
+    return render(request, 'homeapp/about.html')
 
 def contact(request):
-    render(request, 'homeapp/base.html')
+    return render(request, 'homeapp/base.html')
